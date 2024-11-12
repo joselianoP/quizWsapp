ea5b336d2a0a65143f427af5bcde5473ffceff65
joseliano <160750939+joselianoP@users.noreply.github.com>
2024-11-12 19:36:14 -0300

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  version: '1.0.0.18', // Substitua pela versão atual
+  version: '1.0.0.19', // Substitua pela versão atual
   dataUltimaVersao: '12/11/2024',
 };
